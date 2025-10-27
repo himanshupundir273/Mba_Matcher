06df103c829b26daa894d5e34d74fdd05f240789
himanshupundir273 <himanshupundir273@gmail.com>
2025-10-27 21:12:47 +0530

    changes

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,6 +1,5 @@
-require("dotenv").config();
 const API_BASE_URL =
-  process.env.REACT_APP_API_URL || "http://localhost:5000/api";
+  import.meta.env.VITE_API_URL || "http://localhost:5000/api";
 
 export const searchUniversities = async (searchParams) => {
   try {

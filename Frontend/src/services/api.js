8d914ebaad824cb2d475d50ca74f39cbbd33cb83
himanshupundir273 <himanshupundir273@gmail.com>
2025-10-27 22:18:20 +0530

    changes

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,5 +1,5 @@
-const API_BASE_URL =
-  import.meta.env.VITE_API_URL || "http://localhost:5000/api";
+const API_BASE_URL = import.meta.env.VITE_API_URL;
+console.log("API Base URL:", API_BASE_URL);
 
 export const searchUniversities = async (searchParams) => {
   try {
